50c6eeb1e580b9d0e9b27e9938a7489bfdfe9303
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-05-09 15:42:13 -0400

    refactor: Use npm and webpack

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+});
